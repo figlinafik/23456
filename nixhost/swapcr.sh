ccc87fea99d17bf2338d443e3dbc5f060741c1a7
figlinafik <38422997+figlinafik@users.noreply.github.com>
2021-12-26 22:07:25 +0300

    add swap create

--- nixhost/swapcr.sh
+++ nixhost/swapcr.sh
@@ -1,8 +1,14 @@
 #!/nin/bash
-#
-
-# fallocate -l 512M /swapfile
+# we take the name from the file "hardware-configuration.nix"
+# create a file "fallocate" or "dd", take off # 
+# the numerical value depends on the memory of the mouth 512M or G and 8245M = 8.1G
+fallocate -l 512M /swapfile
 # dd if=/dev/zero of=/swapfile bs=1M count=512
-# chmod 600 /swapfile
-# mkswap /swapfile
+# give read rights
+chmod 600 /swapfile
+# create a filesystem
+mkswap /swapfile
+# on files
+swapon /swapfile
+# for SSD, take off #
 # swapon --discard /swapfile
